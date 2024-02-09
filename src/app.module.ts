a67f2f91649bdb36d73f60943e83bc76d4d5495a
valmojr <val_mo_jr@hotmail.com>
2024-02-09 17:24:47 -0300

    added initial scaffold entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { EventModule } from './event/event.module';
+import { ConfigModule } from '@nestjs/config';
+import { GroupModule } from './group/group.module';
+import { HouseModule } from './house/house.module';
+import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local', '.env.dev'],
+    }),
+    EventModule,
+    GroupModule,
+    HouseModule,
+    UserModule,
+    RoleModule,
+  ],
 })
 export class AppModule {}
