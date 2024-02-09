ec4ebdf7fe2e4c96b8f99428cd26005ca97fdf54
valmojr <val_mo_jr@hotmail.com>
2024-02-09 19:22:31 -0300

    added house module dependency

--- src/house/house.module.ts
+++ src/house/house.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HouseService } from './house.service';
 import { HouseController } from './house.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [HouseController],
-  providers: [HouseService],
+  providers: [PrismaService, HouseService],
 })
 export class HouseModule {}
