c70d8f82c39fe4f03a2ca85f0528abedd9f3476a
valmojr <val_mo_jr@hotmail.com>
2024-02-15 18:06:02 -0300

    refactored Event Service to handle event changes and throw error if wrong info is provided

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -4,11 +4,35 @@ import { Event } from '@prisma/client';
 import { randomUUID } from 'crypto';
 import { CreateEventDTO } from './event.type';
 
+function EventChecker(data: Event | CreateEventDTO) {
+  if (!data.name || !data.startDate) {
+    throw new Error('Name and start date are required');
+  }
+
+  if (data.startDate && isNaN(data.startDate.getTime())) {
+    throw new Error('Start date must be a valid date');
+  }
+
+  if (data.startDate && data.endDate) {
+    if (data.startDate > data.endDate) {
+      throw new Error('Start date must be before end date');
+    }
+  }
+
+  const now = new Date();
+  if (data.startDate < now) {
+    throw new Error('Start date must be in the future');
+  }
+
+  return data.name !== undefined || data.startDate !== undefined;
+}
+
 @Injectable()
 export class EventService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(data: CreateEventDTO) {
+    EventChecker(data);
     return await this.prismaService.event.create({
       data: {
         id: `${randomUUID()}`,
@@ -20,6 +44,7 @@ export class EventService {
   }
 
   async upsert(data: Event) {
+    EventChecker(data);
     return await this.prismaService.event.upsert({
       where: {
         id: data.id,
@@ -58,6 +83,7 @@ export class EventService {
   }
 
   async update(updateEvent: Event) {
+    EventChecker(updateEvent);
     return await this.prismaService.event.update({
       where: {
         id: updateEvent.id,
