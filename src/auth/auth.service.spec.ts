d01f3fa5b2dc87fe9791abf17f091be21d6d6c13
valmojr <val_mo_jr@hotmail.com>
2024-02-19 17:42:14 -0300

    created unit tests for the auth service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -3,9 +3,18 @@ import { AuthService } from './auth.service';
 import { UserService } from '../user/user.service';
 import { PrismaService } from '../prisma.service';
 import TestModuleBuilder from '../../test/test.module';
+import { randomUUID } from 'crypto';
+import { sign } from 'jsonwebtoken';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let prisma: PrismaService;
+
+  const testUser = {
+    id: randomUUID(),
+    username: 'test',
+    displayName: 'test',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await TestModuleBuilder({
@@ -13,9 +22,52 @@ describe('AuthService', () => {
     });
 
     service = module.get<AuthService>(AuthService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Sign In function Tests', () => {
+    it('should throw unauthorized exception if wrong auth params are provided', async () => {
+      prisma.user.findUnique = jest.fn().mockReturnValueOnce(null);
+
+      try {
+        await service.signIn(testUser);
+      } catch (error) {
+        expect(error.message).toBe('user not found');
+      }
+    });
+
+    it('should return a valid authorization if the right auth params are provided', async () => {
+      prisma.user.findUnique = jest.fn().mockReturnValueOnce(testUser);
+
+      const result = await service.signIn(testUser);
+
+      expect(result).toMatchObject(testUser);
+    });
+  });
+
+  describe('Identity Checker Tests', () => {
+    const invalidToken = 'invalid-token';
+    const validToken = sign(testUser, process.env.JWT_SECRET);
+
+    it('should not be able to check the identity if the token provided is invalid', async () => {
+      try {
+        const auth = await service.getMe(invalidToken);
+
+        expect(auth).toBe(false);
+      } catch (error) {
+        expect(error.message).toBe('jwt malformed');
+      }
+    });
+    it('should be able to check the identity of a valid token', async () => {
+      prisma.user.findUnique = jest.fn().mockReturnValueOnce(testUser);
+
+      const auth = await service.getMe(validToken);
+
+      expect(auth).toMatchObject(testUser);
+    });
   });
 });
