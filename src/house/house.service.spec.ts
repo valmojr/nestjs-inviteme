59138a9a12585e8d3e11d667a0bc3fccda9a74c9
valmojr <val_mo_jr@hotmail.com>
2024-02-19 21:32:14 -0300

    100% coverage on house service unit tests

--- src/house/house.service.spec.ts
+++ src/house/house.service.spec.ts
@@ -2,9 +2,20 @@ import { TestingModule } from '@nestjs/testing';
 import { HouseService } from './house.service';
 import { PrismaService } from '../prisma/prisma.service';
 import TestModuleBuilder from '../../test/test.module';
+import { House } from '@prisma/client';
+import { randomUUID } from 'crypto';
 
 describe('HouseService', () => {
   let service: HouseService;
+  let prisma: PrismaService;
+
+  const testHouse: House = {
+    id: randomUUID(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    name: 'Test House',
+    avatar: null,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await (
@@ -14,9 +25,69 @@ describe('HouseService', () => {
     ).compile();
 
     service = module.get<HouseService>(HouseService);
+    prisma = module.get<PrismaService>(PrismaService);
+
+    prisma.house.create = jest.fn().mockResolvedValue(testHouse);
+    prisma.house.findUnique = jest.fn().mockResolvedValue(testHouse);
+    prisma.house.findMany = jest.fn().mockResolvedValue([testHouse]);
+    prisma.house.update = jest.fn().mockResolvedValue({
+      ...testHouse,
+      name: 'Updated House',
+      updatedAt: new Date(),
+    });
+    prisma.house.delete = jest.fn().mockResolvedValue({
+      ...testHouse,
+      name: 'Removed House',
+    });
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a house', async () => {
+    const house = await service.create(testHouse);
+
+    expect(house).toHaveProperty('id');
+  });
+
+  it('should find all houses', async () => {
+    const houses = await service.findAll();
+
+    expect(houses).toBeInstanceOf(Array);
+  });
+
+  it('should find a house by id', async () => {
+    const house = await service.findOne(testHouse.id);
+
+    expect(house).toHaveProperty('id');
+  });
+
+  it('should find a house by object', async () => {
+    const house = await service.findOne(testHouse);
+
+    expect(house).toHaveProperty('id');
+  });
+
+  it('should update a house', async () => {
+    const house = await service.update(testHouse);
+
+    expect(house).toHaveProperty('id');
+    expect(house.name).toBe('Updated House');
+    expect(house.updatedAt).not.toEqual(testHouse.updatedAt);
+  });
+
+  it('should remove a house by id', async () => {
+    const house = await service.remove(testHouse.id);
+
+    expect(house).toHaveProperty('id');
+    expect(house.name).toBe('Removed House');
+  });
+
+  it('should remove a house by object', async () => {
+    const house = await service.remove(testHouse);
+
+    expect(house).toHaveProperty('id');
+    expect(house.name).toBe('Removed House');
   });
 });
