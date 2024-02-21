a5bd535db04a615c088d32ea717a374f1cc5c734
valmojr <val_mo_jr@hotmail.com>
2024-02-21 16:05:27 -0300

    added user database integration to AuthGuard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -5,11 +5,16 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private userService: UserService,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
@@ -22,7 +27,13 @@ export class AuthGuard implements CanActivate {
         secret: process.env.AUTH_SECRET as string,
       });
 
-      request['user'] = payload;
+      request['user'] = payload as User;
+
+      const user = await this.userService.findOne(payload);
+
+      if (!user) {
+        throw new UnauthorizedException();
+      }
     } catch {
       throw new UnauthorizedException();
     }
