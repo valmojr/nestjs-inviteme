fdeff738d465896110bd81414a7f4a474cff4ddf
valmojr <val_mo_jr@hotmail.com>
2024-02-21 15:52:25 -0300

    full coverage on auth controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,25 +1,93 @@
 import { TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import TestModuleBuilder from '../../test/test.module';
+import { UnauthorizedException } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
 import { PrismaService } from '../prisma/prisma.service';
-import TestModuleBuilder from '../../test/test.module';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
+
+  const mockUser: User = {
+    id: '1',
+    username: 'test user',
+    displayName: 'Test User',
+    discordId: '1234567890',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    password: null,
+    avatar: null,
+    email: null,
+    bannerColor: null,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await (
       await TestModuleBuilder({
         controllers: [AuthController],
-        providers: [PrismaService, AuthService, UserService],
+        providers: [AuthService, UserService, JwtService, PrismaService],
       })
     ).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('signIn auth function', () => {
+    it('should return the user if signIn is successful', async () => {
+      jest.spyOn(authService, 'signIn').mockResolvedValueOnce(mockUser);
+
+      const result = await controller.signIn(mockUser);
+
+      expect(result).toEqual(mockUser);
+    });
+
+    it('should throw UnauthorizedException if user is not found', async () => {
+      const mockUser = {
+        id: '2',
+        username: 'nonexistent',
+        displayName: 'Nonexistent User',
+      };
+      jest
+        .spyOn(authService, 'signIn')
+        .mockRejectedValueOnce(new UnauthorizedException('user not found'));
+
+      const signIn = () => controller.signIn(mockUser);
+
+      try {
+        expect(signIn).rejects.toThrow(UnauthorizedException);
+      } catch (error) {
+        expect(error.message).toEqual('user not found');
+      }
+    });
+  });
+
+  describe('me auth function', () => {
+    it('should return user details if token is valid', async () => {
+      const mockToken = 'validToken';
+
+      jest.spyOn(authService, 'getMe').mockResolvedValueOnce(mockUser);
+
+      const result = await controller.me({
+        headers: { authorization: `Bearer ${mockToken}` },
+      } as any);
+
+      expect(result).toEqual(mockUser);
+    });
+
+    it('should return false if token is invalid', async () => {
+      const mockToken = 'invalidToken';
+      jest.spyOn(authService, 'getMe').mockResolvedValueOnce(false);
+
+      const result = await controller.me({
+        headers: { authorization: `Bearer ${mockToken}` },
+      } as any);
+
+      expect(result).toEqual(false);
+    });
   });
 });
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from '../prisma/prisma.service';
 import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  providers: [PrismaService, AuthService, UserService],
+  providers: [PrismaService, AuthService, UserService, JwtService],
 })
 export class AuthModule {}
