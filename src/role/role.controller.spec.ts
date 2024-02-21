7d3a31cd0724095799aa47a581fb659b8f8cb85d
valmojr <val_mo_jr@hotmail.com>
2024-02-21 15:51:34 -0300

    full coverage on role controller

--- src/role/role.controller.spec.ts
+++ src/role/role.controller.spec.ts
@@ -3,22 +3,96 @@ import { RoleController } from './role.controller';
 import { RoleService } from './role.service';
 import { PrismaService } from '../prisma/prisma.service';
 import TestModuleBuilder from '../../test/test.module';
+import { UserService } from '../user/user.service';
+import { Role } from '@prisma/client';
+import { randomUUID } from 'crypto';
 
 describe('RoleController', () => {
   let controller: RoleController;
+  let service: RoleService;
 
+  const testRole: Role = {
+    id: randomUUID(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    name: 'test',
+    userId: null,
+    groupID: null,
+  };
   beforeEach(async () => {
     const module: TestingModule = await (
       await TestModuleBuilder({
         controllers: [RoleController],
-        providers: [PrismaService, RoleService],
+        providers: [PrismaService, RoleService, UserService],
       })
     ).compile();
 
     controller = module.get<RoleController>(RoleController);
+    service = module.get<RoleService>(RoleService);
+
+    service.create = jest.fn().mockResolvedValueOnce(testRole);
+    service.findAll = jest.fn().mockResolvedValueOnce([testRole]);
+    service.findOne = jest.fn().mockResolvedValueOnce(testRole);
+    service.update = jest.fn().mockResolvedValueOnce({
+      ...testRole,
+      updatedAt: new Date(),
+      name: 'updated role',
+    });
+    service.remove = jest.fn().mockResolvedValueOnce({
+      ...testRole,
+      name: 'deleted role',
+    });
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be able to create e Role', async () => {
+    const role = await controller.create(testRole);
+
+    expect(role).toBeDefined();
+  });
+
+  it('should be able to findAll roles', async () => {
+    const roles = await controller.findAll();
+
+    expect(roles).toBeDefined();
+    expect(roles).toBeInstanceOf(Array);
+  });
+
+  it('should be able to find one Role', async () => {
+    const role = await controller.findOne(testRole.id);
+
+    expect(role).toBeDefined();
+    expect(role.id).toEqual(testRole.id);
+  });
+
+  it('should be able to update a role by id', async () => {
+    const role = await controller.update(testRole.id, testRole);
+
+    expect(role).toBeDefined();
+    expect(role.name).toEqual('updated role');
+  });
+
+  it('should be able to update a role by object', async () => {
+    const role = await controller.updateOne(testRole);
+
+    expect(role).toBeDefined();
+    expect(role.name).toEqual('updated role');
+  });
+
+  it('should be able to remove a role by id', async () => {
+    const role = await controller.remove(testRole.id);
+
+    expect(role).toBeDefined();
+    expect(role.name).toEqual('deleted role');
+  });
+
+  it('should be able to remove a role by object', async () => {
+    const role = await controller.removeOne(testRole);
+
+    expect(role).toBeDefined();
+    expect(role.name).toEqual('deleted role');
   });
 });
--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { RoleController } from './role.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { UserService } from '../user/user.service';
 
 @Module({
   controllers: [RoleController],
-  providers: [PrismaService, RoleService],
+  providers: [PrismaService, RoleService, UserService],
 })
 export class RoleModule {}
