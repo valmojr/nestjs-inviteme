906a91c744db56edb4dfb97419d1f54f89130396
valmojr <val_mo_jr@hotmail.com>
2024-03-18 17:43:07 -0300

    added cookie parser to bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-
+  app.use(cookieParser());
   process.on('uncaughtException', (err) => {
     console.error('Unhandled Exception:', err); // Log the error details
   });
