2b59c8cf6aaff7146fbfacd5d9812d0b0531d4a4
valmojr <val_mo_jr@hotmail.com>
2024-03-22 23:00:44 -0300

    added banner prop to Userparser function

--- src/util/UserParser.ts
+++ src/util/UserParser.ts
@@ -29,7 +29,7 @@ export async function DiscordUserParser(
     username: discordUser.username,
     displayName: discordUser.displayName,
     bannerColor: discordUser?.banner_color,
-    banner: discordUser.bannerURL({ extension: 'png', forceStatic: true }),
+    banner: discordUser.banner,
     avatar: discordUser.avatar,
     email: userOnDatabase?.email || null,
     password: userOnDatabase?.password || null,
