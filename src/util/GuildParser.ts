b65707013dfaaadee6b11708b3e6a802d58e8366
valmojr <val_mo_jr@hotmail.com>
2024-03-27 19:14:41 -0300

    added prop check to discord guild parser

--- src/util/GuildParser.ts
+++ src/util/GuildParser.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { House } from '@prisma/client';
 import { randomUUID } from 'crypto';
 import { Guild } from 'discord.js';
@@ -6,6 +7,10 @@ import { HouseService } from 'src/house/house.service';
 export default function GuildParser(guild: Guild, isPublic?: boolean): House {
   if (isPublic === undefined) {
     isPublic = false;
+  }
+
+  if (!guild?.id || !guild?.createdAt || !guild?.name) {
+    throw new BadRequestException('invalid guild information provided');
   }
 
   return {
