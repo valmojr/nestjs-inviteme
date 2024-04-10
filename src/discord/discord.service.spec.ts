6d05d48d2bbe0ec42f8da2a1ccbb33228021aef8
valmojr <val_mo_jr@hotmail.com>
2024-04-10 20:11:53 -0300

    added guild handling tests

--- src/discord/discord.service.spec.ts
+++ src/discord/discord.service.spec.ts
@@ -12,9 +12,12 @@ import { DiscordUpdate } from './discord.update.service';
 import { NecordModule } from 'necord';
 import { DiscordConfigService } from './discord.config.service';
 import { JwtService } from '@nestjs/jwt';
+import { Guild } from 'discord.js';
+import { BadRequestException } from '@nestjs/common';
 
 describe('DiscordService', () => {
   let service: DiscordService;
+  let houseService: HouseService;
 
   beforeEach(async () => {
     const module: TestingModule = await (
@@ -40,9 +43,38 @@ describe('DiscordService', () => {
     ).compile();
 
     service = module.get<DiscordService>(DiscordService);
+    houseService = module.get<HouseService>(HouseService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('guildCreator Tests', () => {
+    it('should throw error if the the provided data is not valid', async () => {
+      const invalidData: Guild = undefined;
+
+      try {
+        service.guildCreator(invalidData);
+        expect(service).toThrowErrorMatchingSnapshot(
+          'invalid guild information provided',
+        );
+      } catch (error) {
+        expect(error).toMatchInlineSnapshot(
+          'invalid guild information provided',
+        );
+      }
+    });
+    it('should be able to create a guild', async () => {
+      houseService.create = jest
+        .fn()
+        .mockRejectedValueOnce(new BadRequestException());
+    });
+  });
+
+  describe('guildDelete Tests', () => {
+    it('should throw error if the the provided data is not valid', async () => {});
+    it('should throw error if the the guild is not found', async () => {});
+    it('should be able to delete a guild', async () => {});
   });
 });
