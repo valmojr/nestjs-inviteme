f255f14202b3e8984cdf77aa38427143257abcd5
valmojr <val_mo_jr@hotmail.com>
2024-07-12 16:17:31 -0300

    added Swagger for API Documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,17 @@ async function bootstrap() {
   process.on('uncaughtException', (err) => {
     console.error('Unhandled Exception:', err);
   });
+
+  {
+    const config = new DocumentBuilder()
+      .setTitle('InviteMe Swagger')
+      .setDescription('InviteMe API Docs')
+      .setVersion('0.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
 
   await app.listen(process.env.PORT || 10000);
 }
