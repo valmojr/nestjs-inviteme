dd4569182fb09c0fbe91ce126e74433893f1fb8d
valmojr <val_mo_jr@hotmail.com>
2024-07-12 19:28:08 -0300

    refactored modular dendencies for discord module

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -4,29 +4,27 @@ import { DiscordController } from './discord.controller';
 import { NecordModule } from 'necord';
 import { DiscordConfigService } from './discord.config.service';
 import { DiscordUpdate } from './discord.update.service';
-import { HouseService } from '../house/house.service';
-import { UserService } from '../user/user.service';
-import { PrismaService } from '../prisma/prisma.service';
-import { EventService } from '../event/event.service';
-import { GroupService } from '../group/group.service';
-import { RoleService } from '../role/role.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { EventModule } from 'src/event/event.module';
+import { UserModule } from 'src/user/user.module';
+import { HouseModule } from 'src/house/house.module';
+import { GroupModule } from 'src/group/group.module';
+import { RoleModule } from 'src/role/role.module';
 
 @Module({
   imports: [
     NecordModule.forRootAsync({
       useClass: DiscordConfigService,
     }),
+    PrismaModule,
+    UserModule,
+    HouseModule,
+    EventModule,
+    // TODO - Remove this
+    GroupModule,
+    RoleModule,
   ],
   controllers: [DiscordController],
-  providers: [
-    DiscordService,
-    DiscordUpdate,
-    PrismaService,
-    EventService,
-    GroupService,
-    RoleService,
-    HouseService,
-    UserService,
-  ],
+  providers: [DiscordService, DiscordUpdate],
 })
 export class DiscordModule {}
